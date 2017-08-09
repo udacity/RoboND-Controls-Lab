a97a96a6d71c8369194265e04ad854f32a1dcd81
Brandon Kinman <brandon@kinmantech.org>
2017-08-09 12:02:31 -0700

    initial commit

---
+++ quad_controller/src/quad_controller/pid_controller.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2017 Electric Movement Inc.
+# All Rights Reserved.
+
+# Author: Brandon Kinman
+
+
+class PIDController:
+    def __init__(self, kp = 0.0, ki = 0.0, kd = 0.0, max_windup = 10):
+        #TODO
+        pass
+    def reset(self):
+        #TODO
+        pass
+
+    def setTarget(self, target):
+        #TODO
+        pass
+
+    def setKP(self, kp):
+        #TODO
+        pass
+
+    def setKI(self, ki):
+        #TODO
+        pass
+
+    def setKD(self, kd):
+        #TODO
+        pass
+
+    def setMaxWindup(self, max_windup):
+        #TODO
+        pass
+
+    def update(self, measured_value, timestamp):
+        #TODO
+        pass
+
+
